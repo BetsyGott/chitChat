b6b1372f25ebce7675cbbed269e1147179caacdf
Betsy Gottesman <eightamps@gmail.com>
2016-07-06 23:10:36 -0700

    added almost everything

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -7,10 +7,8 @@ angular.module('angularfireSlackApp')
         };
 
         channelsCtrl.createChannel = function(){
-            channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(){
-                channelsCtrl.newChannel = {
-                    name: ''
-                };
+            channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(ref){
+                $state.go('channels.messages', {channelId: ref.key()});
             });
         };
 
@@ -18,10 +16,21 @@ angular.module('angularfireSlackApp')
         channelsCtrl.channels = channels;
         channelsCtrl.getDisplayName = Users.getDisplayName;
         channelsCtrl.getGravatar = Users.getGravatar;
+        channelsCtrl.users = Users.all;
 
         channelsCtrl.logout = function(){
             Auth.$unauth();
             $state.go('home');
+        };
+
+        Users.setOnline(profile.$id);
+
+        channelsCtrl.logout = function(){
+            channelsCtrl.profile.online = null;
+            channelsCtrl.profile.$save().then(function(){
+                Auth.$unauth();
+                $state.go('home');
+            });
         };
         
     });
--- app/channels/messages.service.js
+++ app/channels/messages.service.js
@@ -2,11 +2,17 @@ angular.module('angularfireSlackApp')
     .factory('Messages', function($firebaseArray, FirebaseUrl){
 
         var channelMessagesRef = new Firebase(FirebaseUrl+'channelMessages');
+        var userMessagesRef = new Firebase(FirebaseUrl+'userMessages');
 
         return {
             forChannel: function(channelId){
-                //gets back all the messages for a specific channel id
                 return $firebaseArray(channelMessagesRef.child(channelId));
+            },
+            forUsers: function(uid1, uid2){
+                var path = uid1 < uid2 ? uid1+'/'+uid2 : uid2+'/'+uid1;
+
+                return $firebaseArray(userMessagesRef.child(path));
             }
         };
+        
     });
